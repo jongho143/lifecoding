49d638834c23e1cbc4ffae141dbb2e25d84ad41c
leexiudev <leexiu.dev@gamil.com>
2020-04-10 19:25:11 +0900

    컴포넌트 이벤트

--- src/App.js
+++ src/App.js
@@ -5,38 +5,55 @@ import Subject from './components/Subject';
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       mode: 'read',
-      subject: {title:'WEB', sub:'World Wide Web!'},
-      wellcome:{title:'Wellcome', desc:'Hello, React!!'},
-      contents:[
-        {id:1, title:'HTML', desc:'HTML is for information'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      selected_content_id: 2,
+      subject: { title: 'WEB', sub: 'World Wide Web!' },
+      wellcome: { title: 'Wellcome', desc: 'Hello, React!!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is for information' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' }
       ]
     }
   }
   render() {
     var _title, _desc = null;
-    if(this.state.mode === 'Wellcome'){
+    if (this.state.mode === 'Wellcome') {
       _title = this.state.wellcome.title;
       _desc = this.state.wellcome.desc;
-    } else if(this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+    } else if (this.state.mode === 'read') {
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
         <Subject
-         title={this.state.subject.title} 
-         sub={this.state.subject.sub}
-         onChangePage={function(){
-           this.setState({mode:'Wellcome'})
-         }.bind(this)}
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: 'Wellcome' })
+          }.bind(this)}
         />
-        <Toc data={this.state.contents}/>
+        <Toc
+          onChangePage={function (id) {
+            this.setState({
+              mode: 'read',
+              selected_content_id: Number(id)
+            });
+
+          }.bind(this)}
+          data={this.state.contents} />
         <Content title={_title} desc={_desc} />
       </div>
     );
