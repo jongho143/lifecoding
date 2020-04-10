e4c46da20e4012980802eb31189485701db1cf7e
leexiudev <leexiu.dev@gamil.com>
2020-04-10 16:45:03 +0900

    이벤트 활용

--- src/App.js
+++ src/App.js
@@ -1,48 +1,43 @@
 import React, { Component } from 'react';
+import Toc from './components/Toc';
+import Content from './components/Content';
+import Subject from './components/Subject';
 
-
-class Subject extends Component {
-  render (){
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class Toc extends Component {
-  render () {
-    return (
-      <ul>
-        <li><a href="1.html">HTML</a></li>
-        <li><a href="2.html">CSS</a></li>
-        <li><a href="3.html">Javascript</a></li>
-      </ul>
-    );
-  }
-}
-
-
-class Content extends Component {
-  render () {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-          {this.props.desc}
-      </article>
-    );
-  }
-}
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      mode: 'read',
+      subject: {title:'WEB', sub:'World Wide Web!'},
+      wellcome:{title:'Wellcome', desc:'Hello, React!!'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
+    }
+  }
   render() {
+    var _title, _desc = null;
+    if(this.state.mode === 'Wellcome'){
+      _title = this.state.wellcome.title;
+      _desc = this.state.wellcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
-      <div>
-        <Subject title="WEB" sub="world wide web!" />
-        <Toc />
-        <Content title="HTML" desc="HTML is HyperText Markup Language." />
+      <div className="App">
+        <Subject
+         title={this.state.subject.title} 
+         sub={this.state.subject.sub}
+         onChangePage={function(){
+           this.setState({mode:'Wellcome'})
+         }.bind(this)}
+        />
+        <Toc data={this.state.contents}/>
+        <Content title={_title} desc={_desc} />
       </div>
     );
   }
